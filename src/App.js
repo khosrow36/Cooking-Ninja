4a44d9416b7e6632ff306c44c53485aa116c07e8
Artur Przystaś <artek.przystas@gmail.com>
2022-07-31 13:25:54 +0200

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+//pages
 import Home from "./pages/home/Home";
 import Create from "./pages/create/Create";
 import Recpie from "./pages/recipe/Recipe";
 import Search from "./pages/search/Search";
-
+//compontents
+import Navbar from "./compontents/Navbar";
+//styles
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
