d6377350573b7015772d31778d91c4b227a3cad1
Artur Przystaś <artek.przystas@gmail.com>
2022-07-31 19:33:54 +0200

    created repcipe list

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,17 @@
+import { useFetch } from "../../hooks/useFetch";
+//components
+import RecipeList from "../../compontents/RecipeList";
+
+//styles
 import "./Home.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
