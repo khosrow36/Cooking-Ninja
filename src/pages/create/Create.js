7e960909053ac986533b77d2afc91177dcbb8fda
Artur Przystaś <artek.przystas@gmail.com>
2022-08-03 15:04:31 +0200

    added create reicipe page

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,50 @@
+import { useState } from "react";
+
 import "./Create.css";
 
 export default function Create() {
-  return <div>Create</div>;
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add new recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe Title</span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+        {/* TODO: add igredients */}
+        <label>
+          <span>Recipe method</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+          <label>
+            <span>Cooking time (minutes)</span>
+            <input
+              type="number"
+              onChange={(e) => setCookingTime(e.target.value)}
+              value={cookingTime}
+              required
+            />
+          </label>
+        </label>
+        <button className="button">Submit</button>
+      </form>
+    </div>
+  );
 }
