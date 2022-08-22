000394928a4998e30456590c015c62baa77ab641
Artur Przystaś <artek.przystas@gmail.com>
2022-08-22 17:04:21 +0200

    added navbar style via context

--- src/compontents/Navbar.js
+++ src/compontents/Navbar.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
 
 import SearchBar from "./SearchBar";
 
 import "./Navbar.css";
 
+import { ThemeContext } from "../context/ThemeContext";
+
 export default function Navbar() {
+  const { color } = useContext(ThemeContext);
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking ninja</h1>
