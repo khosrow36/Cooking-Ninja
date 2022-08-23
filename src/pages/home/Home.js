5bb9e6dc205abcda51ac8ab16a05e50d39687ca6
Artur Przystaś <artek.przystas@gmail.com>
2022-08-23 22:35:05 +0200

    fixed recpies list after deleting recipe

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -13,10 +13,8 @@ export default function Home() {
   useEffect(() => {
     setIsPending(true);
 
-    projectFirestore
-      .collection("recipes")
-      .get()
-      .then((snapshot) => {
+    const unsub = projectFirestore.collection("recipes").onSnapshot(
+      (snapshot) => {
         if (snapshot.empty) {
           setError("No reciepies to show :(");
           setIsPending(false);
@@ -28,11 +26,13 @@ export default function Home() {
           setData(results);
           setIsPending(false);
         }
-      })
-      .catch((err) => {
+      },
+      (err) => {
         setError(err.message);
         setIsPending(false);
-      });
+      }
+    );
+    return () => unsub();
   }, []);
 
   return (
